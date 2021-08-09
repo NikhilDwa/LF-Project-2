8e46a27c14fa4744f2c4597e02ee368a92c64a0c
Roman251 <romanregmi@hotmail.com>
2021-08-09 19:53:44 +0545

    Update Project 2.py

--- Project 2.py
+++ Project 2.py
@@ -13,3 +13,12 @@ print(head)
 
 for index, value in enumerate(data_array[0]):
     print(index, value)
+    
+"""### Dropping irrelevant data points"""
+
+new_data_array = np.delete(data_array, [0, 1, 14, 15, 16, 17, 18, 19, 20], axis=1)
+
+"""### Column headings after dropping some of the column form the dataset."""
+
+print(new_data_array[0])
+
