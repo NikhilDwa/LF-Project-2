1bda7a8512469c64ffad959485d3c22a56fdf4c7
Nikhil Dwa <nikhildwa627@gmail.com>
2021-08-09 19:33:06 +0545

    Looking head

--- Project 2.py
+++ Project 2.py
@@ -4,3 +4,12 @@ import matplotlib.pyplot as plt
 data_array = np.loadtxt("kc_house_data.csv", delimiter = ",", dtype='object')
 
 print(data_array)
+
+print(data_array[0])
+
+head = data_array[0:6,]
+
+print(head)
+
+for index, value in enumerate(data_array[0]):
+    print(index, value)
